746049667e93fd1219efacbd6398670940181f53
junsu <melon8372@gmail.com>
2019-12-16 23:27:53 +0900

    refactor: Separation of responsibility theater and ticketSeller
    Theater에서는 TicketSeller가 Audience에게 표를 판매하는 책임을 가져야하는데, 기존에는 Theater가 그 책임을 가지고 있음.
    
    TicketSeller는 표를 파는 책임을 다른곳에서 쓰이지 않도록 TicketSeller의 TicketOffice를 캡슐화 -> delete getTicketOffice()
    
    Result: 캡술화를 함으로써 TicketSeller의 책임은 TicketSeller 내부에서만 책임을 다하기 때문에 응집도는 높아지고, Theater에서는 단순히 TicketSeller가 TicketOffice의 Ticket까지 가져오는 등 구체적인 내부를 알지 않고, TicketSeller가 Audience에게 표를 판매한다는 것만 수행하기에 TicketSeller와의 결합도는 낮아진다.

--- src/com/junsu/object/TicketSeller.java
+++ src/com/junsu/object/TicketSeller.java
@@ -7,7 +7,15 @@ public class TicketSeller {
     this.ticketOffice = ticketOffice;
   }
 
-  public TicketOffice getTicketOffice() {
-    return ticketOffice;
+  public void sellTo(Audience audience) {
+    if (audience.getBag().hasInvitation()) {
+      Ticket ticket = this.ticketOffice.getTicket();
+      audience.getBag().setTicket(ticket);
+    } else {
+      Ticket ticket = this.ticketOffice.getTicket();
+      audience.getBag().minusAmount(ticket.getFee());
+      this.ticketOffice.plusAmount(ticket.getFee());
+      audience.getBag().setTicket(ticket);
+    }
   }
 }
